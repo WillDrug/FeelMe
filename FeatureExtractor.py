1f6808acd058e67b971cbdc27ec934c378d14222
WillDrug <whydafuqdoineedthis@gmail.com>
2017-03-12 22:39:10 +0300

    Rebase to laptop

---
+++ FeatureExtractor.py
@@ -0,0 +1,66 @@
+# Librosa for audio
+import librosa
+# And the display module for visualization
+import librosa.display
+import numpy as np
+
+#from multiprocessing import Pool, cpu_count
+
+FEATURE_COUNT = int(7)
+FEATURE_NAMES = b'BPM, EQ1, EQ2, EQ3, EQ4, EQ5, EQ6, EQ7, TUNE\n'
+
+def getFeaturesFromList(fls):
+    ftlist = np.zeros((fls.__len__(), FEATURE_COUNT))
+    """
+    pool = Pool(processes=np.min((fls.__len__(), cpu_count())))
+    result = []
+    #for i in range(0, fls.__len__()):
+    result.append(pool.map(getFeaturesFromName, fls))
+    pool.close()
+    pool.join()
+    for i in zip(range(0, fls.__len__()), result):
+        print(i[1])
+        #ftlist[i[0], :] = i[1]
+    print(ftlist)"""
+    for i in range(0, fls.__len__()):
+        ftlist[i, :] = getFeaturesFromName(fls[i])
+    return ftlist
+
+def getFeaturesFromName(fl):
+    y, sr = librosa.load(fl)
+    return getFeatures(y, sr)
+def getFeatures(y, sr):
+    # base vars
+    fts = np.zeros((1, FEATURE_COUNT))
+    hop_length = 512  # ~23ms hops
+
+    # Calculate series
+    y_harmonic, y_percussive = librosa.effects.hpss(y)
+    S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
+    # Convert to log scale (dB). Use the peak power as reference.
+    log_S = librosa.logamplitude(S, ref_power=np.max)
+    C = librosa.feature.chroma_cqt(y=y_harmonic, sr=sr)
+
+    # Calculate features
+    tempo, beats = librosa.beat.beat_track(y=y_percussive, sr=sr)
+    tuning = librosa.estimate_tuning(y=y_harmonic, sr=sr)
+    onsets_harmonic = librosa.onset.onset_detect(y=y_harmonic,
+                                                 sr=sr,
+                                                 hop_length=hop_length)
+    contrast = librosa.feature.spectral_contrast(S=log_S, sr=sr)
+    avg_bands = np.mean(contrast, axis=1)
+    mean_notes = np.mean(C, axis=1)
+
+    # Map features
+    fts[0, 0] = tempo
+    fts[0, 1] = tuning
+    fts[0, 2] = onsets_harmonic.__len__()
+    fts[0, 3:6] = mean_notes.argsort()[-3:][::-1]
+
+    return fts
+def saveFeatures(fts, fn='features.txt'):
+    with open('final.csv', 'wb') as f:
+        f.write(FEATURE_NAMES)
+    # f.write(bytes("SP,"+lists+"\n","UTF-8"))
+    # Used this line for a variable list of numbers
+        numpy.savetxt(f, a, delimiter=",")
--- FeelMe.py
+++ FeelMe.py
@@ -1,3 +1,70 @@
+from __future__ import print_function
+import FeatureExtractor as FE
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+from datetime import datetime
+startTime = datetime.now()
+
+# AUX IMPORTS
 import librosa
 
-y, sr = librosa.load('I:\Mixing\Cairo\Cairo.mp3')
+import matplotlib.pyplot as plt
+import matplotlib.style as ms
+ms.use('seaborn-muted')
+import IPython.display
+import librosa.display
+
+# FOR DEBUG
+import pickle
+
+# END AUX IMPORTS
+
+# GET MAIN AUDIO LIST
+
+musicpath = 'F:\Work\AuPy\FeelMe\\testSet'
+inlabels = [f for f in listdir(musicpath) if isfile(join(musicpath, f))]
+inmusic = [musicpath+'\\'+f for f in listdir(musicpath) if isfile(join(musicpath, f))]
+#fts = FE.getFeaturesFromList(inmusic)
+sclabels = ["o","v","^","<",">"]
+
+
+for i, l, m in zip(inmusic, inlabels, sclabels):
+    y, sr = librosa.load(i)
+    y_harmonic, y_percussive = librosa.effects.hpss(y)
+    # harmonic
+    S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128)
+    log_S = librosa.logamplitude(S, ref_power=np.max)
+    #C = librosa.feature.chroma_cqt(y=y_percussive, sr=sr)
+    plt.figure()
+    C = librosa.cqt(y, sr=sr)
+    librosa.display.specshow(librosa.amplitude_to_db(C, ref=np.max),sr=sr, x_axis='time', y_axis='cqt_note')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title(l)
+    plt.tight_layout()
+"""
+#pickle._dump(fts, open('fts.txt', 'wb+'))
+fts = pickle.load(open('fts.txt', 'rb+'))
+
+
+# SCATTER FEATURES
+ftss = fts[:, 0:3]
+fcnt = ftss.shape[1]
+
+f, axarr = plt.subplots(fcnt, fcnt)
+for i in range(0, fcnt):
+    for j in range(0, fcnt):
+        if i==j: continue
+        axarr[i, j].scatter(ftss[:, i], ftss[:, j])
+        axarr[i, j].set_title("X: {}, Y: {}".format(i, j))
+        for label, x, y in zip(inlabels, ftss[:, i], ftss[:, j]):
+            axarr[i, j].annotate(
+                label,
+                xy=(x, y), xytext=(-20, 20),
+                textcoords='offset points', ha='right', va='bottom',
+                bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5),
+                arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0'))
+f.subplots_adjust(hspace=1.0)
+"""
+print("Process {} at: {}".format(__name__, datetime.now() - startTime))
+plt.show()
