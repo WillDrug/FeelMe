90eea9b996914876ed4a597a9e079a8018d291d3
WillDrug <whydafuqdoineedthis@gmail.com>
2017-04-24 22:49:32 +0300

    trying to create architecture

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -4,48 +4,112 @@ import librosa
 import librosa.display
 import numpy as np
 
-#from multiprocessing import Pool, cpu_count
+# FEATURE INITIALIZATION
 
-FEATURE_COUNT = int(7)
-FEATURE_NAMES = b'BPM, EQ1, EQ2, EQ3, EQ4, EQ5, EQ6, EQ7, TUNE\n'
+FEATURE_COUNT = int(1070)
+FEATURE_NAMES = b'label, est.BPM.perc,est.BPM.harm,est.BPM.clean.perc,est.BPM.clean.harm,har.div.perc.full,'
+for i in range(0, 1025):
+    FEATURE_NAMES += b'Spectrogram.db.'+bytes("{},".format(i), encoding='utf-8')
+for i in range(0, 6):
+    FEATURE_NAMES += b'tonnetz.'+bytes("{},".format(i), encoding='utf-8')
+for i in range(0, 12):
+    FEATURE_NAMES += b'CCQT.'+bytes("{},".format(i), encoding='utf-8')
+for i in range(0, 20):
+    FEATURE_NAMES += b'MFCC.'+bytes("{},".format(i), encoding='utf-8')
+FEATURE_NAMES += b'zerocross.median'
+FEATURE_NAMES += b'\n'
 
-def getFeaturesFromList(fls):
-    ftlist = np.zeros((fls.__len__(), FEATURE_COUNT))
-    """
-    pool = Pool(processes=np.min((fls.__len__(), cpu_count())))
-    result = []
-    #for i in range(0, fls.__len__()):
-    result.append(pool.map(getFeaturesFromName, fls))
-    pool.close()
-    pool.join()
-    for i in zip(range(0, fls.__len__()), result):
-        print(i[1])
-        #ftlist[i[0], :] = i[1]
-    print(ftlist)"""
-    for i in range(0, fls.__len__()):
-        ftlist[i, :] = getFeaturesFromName(fls[i])
+def normalize(S):
+    return (S-np.min(S))/(np.max(S)-np.min(S))
+
+def fullNorm(S):
+    for col in range(S.shape[1]):
+        S[:, col] = normalize(S[:, col])
+    return S
+
+def get_features_from_list(musicpath, picklepath, labels, mode, cache=False):
+    ftlist = np.zeros((labels.__len__(), FEATURE_COUNT))
+
+    for row, label in zip(range(0, labels.__len__()), labels):
+        ftlist[row, :] = get_features_from_name(musicpath, picklepath, label, mode, cache)
     return ftlist
 
-def getFeaturesFromName(fl):
-    y, sr = librosa.load(fl)
-    return getFeatures(y, sr)
-def getFeatures(y, sr):
+def get_features_from_name(musicpath, picklepath, label, mode, cache=False):
+    if mode == 'music_plus':
+        try:
+            y, sr = librosa.load(musicpath + '\\' + label)
+        except FileNotFoundError:
+            exit(2)
+    elif mode == 'picled':
+        try:
+            y, sr = pickle.load(open(picklepath + '\\' + label + '.pic', 'rb+'))
+        except FileNotFoundError:
+            exit(2)
+    else:
+        try:
+            y, sr = pickle.load(open(picklepath + '\\' + label + '.pic', 'rb+'))
+        except FileNotFoundError:
+            y, sr = librosa.load(musicpath + '\\' + label)
+            if cache:
+                pickle._dump((y, sr), open(picklepath + '\\' + label + '.pic', 'wb+'))
+
+    return getFeatures(y, sr, label)
+
+def getFeatures(y, sr, label):
     # base vars
     fts = np.zeros((1, FEATURE_COUNT))
-    hop_length = 512  # ~23ms hops
 
-    # Calculate series
-    y_harmonic, y_percussive = librosa.effects.hpss(y)
-    # Convert to log scale (dB). Use the peak power as reference.
-    S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=8000)
-    S_log = librosa.logamplitude(S=S, ref=np.min)
+    # signal processing
+    y_h, y_p = librosa.effects.hpss(y)
+    y_h_clean, y_p_clean = librosa.effects.hpss(y, margin=(20, 20))
 
 
+    # feature computation
+    onset_p = librosa.onset.onset_detect(y=y_p, sr=sr, units='time')
+    onset_p_d = np.diff(onset_p)
+    onset_h = librosa.onset.onset_detect(y=y_h, sr=sr, units='time')
+    onset_h_d = np.diff(onset_h)
 
+    onset_p_clean = librosa.onset.onset_detect(y=y_p, sr=sr, units='time')
+    onset_p_clean_d = np.diff(onset_p_clean)
+    onset_h_clean = librosa.onset.onset_detect(y=y_h, sr=sr, units='time')
+    onset_h_clean_d = np.diff(onset_h_clean)
+
+    tmp = librosa.power_to_db(np.abs(librosa.stft(y_h_clean)) ** 2, ref=np.min)
+    ShCdb = np.sum(tmp, axis=1) / tmp.shape[1]
+    tmp = librosa.power_to_db(np.abs(librosa.stft(y_p_clean)) ** 2, ref=np.min)
+    SpCdb = np.sum(tmp, axis=1) / tmp.shape[1]
+
+    SfCdb = librosa.power_to_db(np.abs(librosa.stft(y_p_clean + y_h_clean)) ** 2, ref=np.min)
+
+    tonnetz_full = librosa.feature.tonnetz(y=y, sr=sr)
+
+    ccqt = librosa.feature.chroma_cqt(y=y, sr=22050)
+
+    mfcc = librosa.feature.mfcc(y=y, sr=22050, S=None, n_mfcc=20)
+
+
+    # feature mapping
+    fts[0, 0] = label
+    fts[0, 1] = 60/np.mean(onset_p_d)
+    fts[0, 2] = 60/np.mean(onset_h_d)
+    fts[0, 3] = 60/np.mean(onset_p_clean_d)
+    fts[0, 4] = 60/np.mean(onset_h_clean_d)
+    fts[0, 5] = np.sum(SpCdb)/np.sum(ShCdb)
+    fts[0, 6:1031] = np.sum(SfCdb, axis=1) / tmp.shape[1]
+    fts[0, 1031:1037] = np.sum(tonnetz_full, axis=1)/tonnetz_full.shape[1]
+    fts[0, 1038:1049] = normalize(np.sum(ccqt, axis=1))
+    fts[0, 1050:1070] = np.sum(mfcc, axis=1)
+    fts[0, 1071] = np.median(librosa.feature.zero_crossing_rate(y=y, frame_length=2048, hop_length=512, center=True))
     return fts
-def saveFeatures(fts, fn='features.txt'):
-    with open('final.csv', 'wb') as f:
-        f.write(FEATURE_NAMES)
-    # f.write(bytes("SP,"+lists+"\n","UTF-8"))
-    # Used this line for a variable list of numbers
-        numpy.savetxt(f, a, delimiter=",")
+
+def save_features(fts, fn='features.txt'):
+    try:
+        with open(fn, 'wb') as f:
+            f.write(FEATURE_NAMES)
+            np.savetxt(f, fts, delimiter=",")
+        return True
+    except Exception:
+        return False
+def load_features(fname):
+    print('Stub for loading features from file')
--- FeelMe.py
+++ FeelMe.py
@@ -1,74 +1,122 @@
+# MAIN IMPORTS
 from __future__ import print_function
-
-from datetime import datetime
+import sys
 from os import listdir
 from os.path import isfile, join
 
 import numpy as np
-
-startTime = datetime.now()
-
-# AUX IMPORTS
-import librosa
-from librosa import display
-import matplotlib.pyplot as plt
-
-
-# FOR DEBUG
 import pickle
-
+import FeatureExtractor as FE
 
 # END AUX IMPORTS
 
+#DEF FUNCTIONS
+from datetime import datetime
+startTime = datetime.now()
+
+#BASE VARS
+MUSIC_PATH = 'testSet'
+PICKLE_PATH = 'pickles'
+LOADBY = 'music_plus'
+K_PROC = 'trainK'
+SAVE_K = 'True'
+POST_PROC = 'create_playlists'
+REC = False
+
+#PARSE COMMAND LINE
+import argparse
+pa = argparse.ArgumentParser()
+pa.add_argument('--loadby')
+pa.add_argument('--musicpath')
+pa.add_argument('--picklepath')
+pa.add_argument('--Kpath')
+pa.add_argument('--cache')
+pa.add_argument('--features')
+pa.add_argument('--K_proc')
+pa.add_argument('--saveK')
+pa.add_argument('--post_proc')
+pa.add_argument('--r')
+args = pa.parse_args()
+
+#POPULATE VARIABLES
+loadby = getattr(args, 'loadby')
+if loadby is None:
+    loadby = LOADBY #music, pickles, music_plus, single_classify, features
+
+musicpath = getattr(args, 'musicpath')
+if musicpath is None:
+    musicpath = MUSIC_PATH
+
+picklepath = getattr(args, 'picklepath')
+if picklepath is None:
+    picklepath = PICKLE_PATH
+
+Kpath = getattr(args, 'Kpath')
+
+cache = getattr(args, 'cache')
+if cache == 'True':
+    cache = True
+else:
+    cache = False
+
+feature_file = getattr(args, 'features')
+if feature_file is None:
+    feature_file = 'features.csv'
+
+K_proc = getattr(args, 'K_proc') #preload
+if K_proc is None:
+    K_proc = K_PROC
+
+save_K = getattr(args, 'saveK')
+if save_K is 'True':
+    save_K = True
+else:
+    save_K = False
+
+post_proc = getattr(args, 'post_proc')
+if post_proc is None:
+    post_proc = POST_PROC
+
+rec = getattr(args, 'r')
+if rec == 'True':
+    rec = True
+else:
+    rec = REC
+
 # GET MAIN AUDIO LIST
-
-musicpath = 'testSet'
-inlabels = [f for f in listdir(musicpath) if isfile(join(musicpath, f))]
-inmusic = [musicpath + '\\' + f for f in listdir(musicpath) if isfile(join(musicpath, f))]
-# fts = FE.getFeaturesFromList(inmusic)
-
-
-X = []
-Y = []
-
-for i, l in zip(inmusic, inlabels):
-    try:
-        y, sr = pickle.load(open('pickles/'+l+'.pic', 'rb+'))
-    except FileNotFoundError:
-        y, sr = librosa.load(i)
-        pickle._dump((y, sr), open('pickles/'+l+'.pic', 'wb+'))
-
-    #y_harmonic, y_percussive = librosa.effects.hpss(y, margin=(1.0, 3.0))
-
-    onset_envelope = librosa.onset.onset_strength(y=y, sr=sr, S=None, lag=1, max_size=1, detrend=False, center=True, feature=None, aggregate=None, centering=None)
-    onset_frames = librosa.onset.onset_detect(onset_envelope=onset_envelope, sr=sr)
-    times = librosa.frames_to_time(np.arange(len(onset_envelope)), sr=sr)
-
-    plt.figure()
-    plt.title(l)
-    plt.plot(times, onset_envelope, label='Onset strength')
-    plt.vlines(times[onset_frames], 0, onset_envelope.max(), color='r', alpha=0.9, linestyle='--', label='Onsets')
-    plt.show()
-    exit()
-    #X.append(1)
-    #Y.append(tempo)
-    print(l)
+if loadby in ['music', 'pickles', 'music_plus']:
+    inlabels = [f for f in listdir(musicpath) if isfile(join(musicpath, f))]
+    inmusic = [musicpath + '\\' + f for f in listdir(musicpath) if isfile(join(musicpath, f))]
+    fts = FE.get_features_from_list(musicpath, picklepath, inlabels, loadby, cache)
+elif loadby == 'single_classify':
+    if Kpath is None:
+        print('K-centers need to be specified when classifying one track')
+        exit(1)
+    print('Stub for single classify')
+elif loadby == 'features':
+    fts = FE.load_features(musicpath)
+else:
+    print('Specify load type')
+    exit(2)
 
 
-    # percussive*100/full ??
-    # ONSET count / frame count = something like BPM?
-    # find a way to compute WUBS
-    # something from mean bands - for instance "L, M, H 0/1"
-"""
-plt.scatter(X, Y)
-for label, x, y in zip(inlabels, X, Y):
-            plt.annotate(
-                label,
-                xy=(x, y), xytext=(-20, 20),
-                textcoords='offset points', ha='right', va='bottom',
-                bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5),
-                arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0'))
+if K_proc == 'loadK':
+    print('Stub for loading K')
+elif K_proc == 'trainK':
+    print('Stub for training K')
+else:
+    print('Specify K type')
 
-"""
+if save_K is True:
+    print('Stub for saving K')
+
+if post_proc == 'create_playlists':
+    print('Stub for playlist generation')
+elif post_proc == 'save target':
+    print('Stub for Y saving')
+else:
+    print('Stub for command console for classyfication by shit')
+
+
+
 print("Process {} at: {}".format(__name__, datetime.now() - startTime))
-plt.show()
