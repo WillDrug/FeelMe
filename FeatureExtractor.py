257b70a13ec2cc8f6533d99360ac796b2f144c53
WillDrug <whydafuqdoineedthis@gmail.com>
2017-04-26 15:01:17 +0300

    trying to create architecture

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -1,13 +1,17 @@
 # Librosa for audio
 import librosa
+from audioread import NoBackendError
 # And the display module for visualization
 import librosa.display
 import numpy as np
+import pickle
+import sys
+import os
 
 # FEATURE INITIALIZATION
 
-FEATURE_COUNT = int(1070)
-FEATURE_NAMES = b'label, est.BPM.perc,est.BPM.harm,est.BPM.clean.perc,est.BPM.clean.harm,har.div.perc.full,'
+FEATURE_COUNT = int(1073)
+FEATURE_NAMES = b'#label,est.BPM.perc,est.BPM.harm,est.BPM.clean.perc,est.BPM.clean.harm,har.div.perc.full,'
 for i in range(0, 1025):
     FEATURE_NAMES += b'Spectrogram.db.'+bytes("{},".format(i), encoding='utf-8')
 for i in range(0, 6):
@@ -19,43 +23,77 @@ for i in range(0, 20):
 FEATURE_NAMES += b'zerocross.median'
 FEATURE_NAMES += b'\n'
 
+
+# VARIABLE INITIALIZATION
+PICKLE_PATH = '\\pickles'
+MUSIC_PATH = '\\testSet'
+RECURSIVE = False
+CACHE = False
+FFILE = 'features.fmf'
+
+def normalize_path(PATH):
+    if not PATH.__contains__(':'):
+        if PATH[0] != '\\' and PATH[0] != '/':
+            PATH = '\\' + PATH
+        path = os.path.dirname(os.path.realpath('__file__')) + PATH
+    else:
+        path = PATH
+    return path
+def create_list():
+    fls = []
+    musicpath = normalize_path(MUSIC_PATH)
+    if RECURSIVE:
+        for root, dirs, files in os.walk(musicpath):
+            for file in files:
+                files[files.index(file)] = root + '\\' + file
+            fls.extend(files)
+    else:
+        onlyfiles = [f for f in os.listdir(musicpath) if os.path.isfile(os.path.join(musicpath, f))]
+        for file in onlyfiles:
+            onlyfiles[onlyfiles.index(file)] = musicpath + '\\' + file
+        fls.extend(onlyfiles)
+    return fls
+
 def normalize(S):
     return (S-np.min(S))/(np.max(S)-np.min(S))
 
-def fullNorm(S):
+def full_norm(S):
     for col in range(S.shape[1]):
         S[:, col] = normalize(S[:, col])
     return S
 
-def get_features_from_list(musicpath, picklepath, labels, mode, cache=False):
-    ftlist = np.zeros((labels.__len__(), FEATURE_COUNT))
+def get_features_from_list(fls):
+    ftlist = np.ndarray((0, FEATURE_COUNT))
+    labels = np.ndarray((0, 1))
+    for fl in fls:
+        ft, label = load_and_extract(fl)
+        if ft is not None:
+            ftlist = np.vstack((ftlist, ft))
+            labels = np.vstack((labels, label))
+    return ftlist, labels
 
-    for row, label in zip(range(0, labels.__len__()), labels):
-        ftlist[row, :] = get_features_from_name(musicpath, picklepath, label, mode, cache)
-    return ftlist
+def load_signal(f, picklepath, cache=False):
+    try:
+        y, sr = librosa.load(f)
+        if cache:
+            pickle._dump((y, sr), open(picklepath + '\\' + os.path.basename(f) + '.pic', 'wb+'))
+        return y, sr
+    except NoBackendError:
+        print("> NoBackEndError for {}".format(f))
+        return False, False
+def load_and_extract(fl):
+    picklepath = normalize_path(PICKLE_PATH)
+    try:
+        y, sr = pickle.load(open(picklepath + '\\' + os.path.basename(fl) + '.pic', 'rb+'))
+    except FileNotFoundError:
+        print("> FileNotFoundError for {}".format(f))
+        y, sr = load_signal(fl, picklepath, CACHE)
+        if y==False:
+            return None
 
-def get_features_from_name(musicpath, picklepath, label, mode, cache=False):
-    if mode == 'music_plus':
-        try:
-            y, sr = librosa.load(musicpath + '\\' + label)
-        except FileNotFoundError:
-            exit(2)
-    elif mode == 'picled':
-        try:
-            y, sr = pickle.load(open(picklepath + '\\' + label + '.pic', 'rb+'))
-        except FileNotFoundError:
-            exit(2)
-    else:
-        try:
-            y, sr = pickle.load(open(picklepath + '\\' + label + '.pic', 'rb+'))
-        except FileNotFoundError:
-            y, sr = librosa.load(musicpath + '\\' + label)
-            if cache:
-                pickle._dump((y, sr), open(picklepath + '\\' + label + '.pic', 'wb+'))
+    return extract_features(y, sr, fl)
 
-    return getFeatures(y, sr, label)
-
-def getFeatures(y, sr, label):
+def extract_features(y, sr, label):
     # base vars
     fts = np.zeros((1, FEATURE_COUNT))
 
@@ -90,26 +128,66 @@ def getFeatures(y, sr, label):
 
 
     # feature mapping
-    fts[0, 0] = label
-    fts[0, 1] = 60/np.mean(onset_p_d)
-    fts[0, 2] = 60/np.mean(onset_h_d)
-    fts[0, 3] = 60/np.mean(onset_p_clean_d)
-    fts[0, 4] = 60/np.mean(onset_h_clean_d)
-    fts[0, 5] = np.sum(SpCdb)/np.sum(ShCdb)
-    fts[0, 6:1031] = np.sum(SfCdb, axis=1) / tmp.shape[1]
+    fts[0, 0] = 60/np.mean(onset_p_d)
+    fts[0, 1] = 60/np.mean(onset_h_d)
+    fts[0, 2] = 60/np.mean(onset_p_clean_d)
+    fts[0, 3] = 60/np.mean(onset_h_clean_d)
+    fts[0, 4] = np.sum(SpCdb)/np.sum(ShCdb)
+    fts[0, 5:1030] = np.sum(SfCdb, axis=1) / tmp.shape[1]
     fts[0, 1031:1037] = np.sum(tonnetz_full, axis=1)/tonnetz_full.shape[1]
-    fts[0, 1038:1049] = normalize(np.sum(ccqt, axis=1))
-    fts[0, 1050:1070] = np.sum(mfcc, axis=1)
-    fts[0, 1071] = np.median(librosa.feature.zero_crossing_rate(y=y, frame_length=2048, hop_length=512, center=True))
-    return fts
+    fts[0, 1038:1050] = normalize(np.sum(ccqt, axis=1))
+    fts[0, 1051:1071] = np.sum(mfcc, axis=1)
+    fts[0, 1072] = np.median(librosa.feature.zero_crossing_rate(y=y, frame_length=2048, hop_length=512, center=True))
 
-def save_features(fts, fn='features.txt'):
+    return fts, label
+
+def save_features(fts, labels):
     try:
-        with open(fn, 'wb') as f:
+        with open(FFILE, 'wb') as f:
             f.write(FEATURE_NAMES)
-            np.savetxt(f, fts, delimiter=",")
+            np.savetxt(f, np.c_[labels, fts], delimiter=",", fmt="%s")
         return True
-    except Exception:
+    except Exception as e:
+        print('> Exception occurred while saving stuff: {}'.format(e))
         return False
 def load_features(fname):
-    print('Stub for loading features from file')
+    labels = np.loadtxt(open(FFILE, 'rb'), dtype=str, comments='#', delimiter=',', skiprows=0, usecols=np.arange(0, 1))
+    fts = np.atleast_2d(np.loadtxt(open(FFILE, 'rb'), dtype=float, comments='#', delimiter=',', skiprows=0, usecols=np.arange(1, FEATURE_COUNT+1)))
+    return labels, fts
+
+
+# COMM LINE PARMS
+import argparse
+import sys
+if sys.argv[0]=='FeatureExtractor.py':
+    pa = argparse.ArgumentParser()
+    pa.add_argument('--cachepath', help='Path to cache to and look-up from')
+    pa.add_argument('--musicroot', help='Root folder with music')
+    pa.add_argument('--recursive', help='Parse subfolders')
+    pa.add_argument('--cache', help='If y music will be cached to --cachepath')
+    pa.add_argument('--do', help='Specify action. save - parses music and saves features. \ncache - parses music and just pickles it. \n')
+    pa.add_argument('--ffile', help='Feature file to save features to. Sort of.')
+    args = pa.parse_args()
+    if getattr(args, 'cachepath') is not None:
+        PICKLE_PATH = getattr(args, 'cacheto')
+    if getattr(args, 'musicroot'):
+        MUSIC_PATH = getattr(args, 'musicroot')
+    if getattr(args, 'recursive') == 'y':
+        RECURSIVE = True
+    else:
+        RECURSIVE = False
+    if getattr(args, 'cache') == 'y':
+        CACHE = True
+    else:
+        CACHE = False
+    if getattr(args, 'ffile') is not None:
+        FFILE = getattr(args, 'ffile')
+    act = getattr(args, 'do')
+    if act == 'save':
+        fts, labels = get_features_from_list(create_list())
+        fls = save_features(fts, labels)
+    elif act == 'cache':
+        for file in create_list():
+            load_signal(file, normalize_path(PICKLE_PATH), True)
+    elif act == 'load':
+        fts, labels = load_features(FFILE)
--- FeelMe.py
+++ FeelMe.py
@@ -1,8 +1,6 @@
 # MAIN IMPORTS
 from __future__ import print_function
-import sys
-from os import listdir
-from os.path import isfile, join
+
 
 import numpy as np
 import pickle
@@ -14,109 +12,57 @@ import FeatureExtractor as FE
 from datetime import datetime
 startTime = datetime.now()
 
-#BASE VARS
-MUSIC_PATH = 'testSet'
-PICKLE_PATH = 'pickles'
-LOADBY = 'music_plus'
-K_PROC = 'trainK'
-SAVE_K = 'True'
-POST_PROC = 'create_playlists'
-REC = False
 
 #PARSE COMMAND LINE
 import argparse
 pa = argparse.ArgumentParser()
-pa.add_argument('--loadby')
-pa.add_argument('--musicpath')
-pa.add_argument('--picklepath')
-pa.add_argument('--Kpath')
-pa.add_argument('--cache')
-pa.add_argument('--features')
-pa.add_argument('--K_proc')
-pa.add_argument('--saveK')
-pa.add_argument('--post_proc')
-pa.add_argument('--r')
+# FE FEATURES
+pa.add_argument('--cachepath', help='Path to cache to and look-up from')
+pa.add_argument('--musicroot', help='Root folder with music')
+pa.add_argument('--recursive', help='Parse subfolders')
+pa.add_argument('--cache', help='If y music will be cached to --cachepath')
+pa.add_argument('--ffile', help='Feature file to save features to. Sort of.')
+if getattr(args, 'cachepath') is not None:
+    FE.PICKLE_PATH = getattr(args, 'cacheto')
+if getattr(args, 'musicroot'):
+    FE.MUSIC_PATH = getattr(args, 'musicroot')
+if getattr(args, 'recursive') == 'y':
+    FE.RECURSIVE = True
+else:
+    FE.RECURSIVE = False
+if getattr(args, 'cache') == 'y':
+    FE.CACHE = True
+else:
+    FE.CACHE = False
+if getattr(args, 'ffile') is not None:
+    FE.FFILE = getattr(args, 'ffile')
+act = getattr(args, 'do')
+    if act == 'save':
+        fts, labels = get_features_from_list(create_list())
+        fls = save_features(fts, labels)
+    elif act == 'cache':
+        for file in create_list():
+            load_signal(file, normalize_path(PICKLE_PATH), True)
+    elif act == 'load':
+        fts, labels = load_features(FFILE)
+# K-MEANS FEATURES
+
+# POST_PROCESSING FEATURES
+
 args = pa.parse_args()
-
-#POPULATE VARIABLES
-loadby = getattr(args, 'loadby')
-if loadby is None:
-    loadby = LOADBY #music, pickles, music_plus, single_classify, features
-
-musicpath = getattr(args, 'musicpath')
-if musicpath is None:
-    musicpath = MUSIC_PATH
-
-picklepath = getattr(args, 'picklepath')
-if picklepath is None:
-    picklepath = PICKLE_PATH
-
-Kpath = getattr(args, 'Kpath')
-
-cache = getattr(args, 'cache')
-if cache == 'True':
-    cache = True
+if getattr(args, 'cachepath') is not None:
+    FE.PICKLE_PATH = getattr(args, 'cacheto')
+if getattr(args, 'musicroot'):
+    FE.MUSIC_PATH = getattr(args, 'musicroot')
+if getattr(args, 'recursive') == 'y':
+    FE.RECURSIVE = True
 else:
-    cache = False
-
-feature_file = getattr(args, 'features')
-if feature_file is None:
-    feature_file = 'features.csv'
-
-K_proc = getattr(args, 'K_proc') #preload
-if K_proc is None:
-    K_proc = K_PROC
-
-save_K = getattr(args, 'saveK')
-if save_K is 'True':
-    save_K = True
+    FE.RECURSIVE = False
+if getattr(args, 'cache') == 'n':
+    FE.CACHE = False
 else:
-    save_K = False
+    FE.CACHE = True
 
-post_proc = getattr(args, 'post_proc')
-if post_proc is None:
-    post_proc = POST_PROC
-
-rec = getattr(args, 'r')
-if rec == 'True':
-    rec = True
-else:
-    rec = REC
-
-# GET MAIN AUDIO LIST
-if loadby in ['music', 'pickles', 'music_plus']:
-    inlabels = [f for f in listdir(musicpath) if isfile(join(musicpath, f))]
-    inmusic = [musicpath + '\\' + f for f in listdir(musicpath) if isfile(join(musicpath, f))]
-    fts = FE.get_features_from_list(musicpath, picklepath, inlabels, loadby, cache)
-elif loadby == 'single_classify':
-    if Kpath is None:
-        print('K-centers need to be specified when classifying one track')
-        exit(1)
-    print('Stub for single classify')
-elif loadby == 'features':
-    fts = FE.load_features(musicpath)
-else:
-    print('Specify load type')
-    exit(2)
-
-
-if K_proc == 'loadK':
-    print('Stub for loading K')
-elif K_proc == 'trainK':
-    print('Stub for training K')
-else:
-    print('Specify K type')
-
-if save_K is True:
-    print('Stub for saving K')
-
-if post_proc == 'create_playlists':
-    print('Stub for playlist generation')
-elif post_proc == 'save target':
-    print('Stub for Y saving')
-else:
-    print('Stub for command console for classyfication by shit')
-
-
+print(FE.create_list())
 
 print("Process {} at: {}".format(__name__, datetime.now() - startTime))
